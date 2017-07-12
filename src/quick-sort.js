9e233523212120d441915c97784838a4907316e6
Ashwin Prakash <Ashwin_Prakash@epam.com>
2017-07-12 13:36:37 +0530

    Ashwin qs implementation

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -1,6 +1,20 @@
 'use strict';
 
 function quickSort(input) {
-
+	
+if (input instanceof Array) {
+  alert('value is Array!');
+} else {
+  alert('Not an array');
+}
+if (input[index] =="") {
+	var inputs = new Array();
+  return inputs;
+}
+if (input.length == 1) {
+	var inputs = new Array();
+  return inputs;
+}	
+	
 };
 module.exports = quickSort;
